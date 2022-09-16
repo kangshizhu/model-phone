da1f16f2e877fdbf4f335c8d75adbb67cca6fd41
635945671@qq.com <635945671>
2022-09-16 09:14:36 +0800

    所有东西

--- src/main/java/modules/entity/BaseEntity.java
+++ src/main/java/modules/entity/BaseEntity.java
@@ -13,22 +13,23 @@ import java.util.Date;
 @Data
 public class BaseEntity {
 
-    /**创建时间*/
-    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    private Date createTime;
-
     /**创建人*/
     private String createBy;
 
     /**修改人*/
     private String updateBy;
 
+    /**创建时间*/
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+
     /**修改时间*/
     @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
-    /**删除标识*/
-    private Integer delFlag;
+//    /**删除标识*/
+//    private Integer delFlag;
 }
